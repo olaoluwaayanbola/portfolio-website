ee795ee77cccb80849f1e66ac11f8c4046e9abdf
Ayanbola <olaoluwaayanbola@gmail.com>
2022-07-02 00:48:41 +0100

    commit

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: 'MERN Memories',
     description: "Using React, Node.js, Express & MongoDB i learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
       image: '/images/1.png',
-      tags: ['MongoDB',"Mongoose", 'Express', 'React', 'Node'],
+      tags: ['MongoDB',"Mongoose", 'Express', 'React']
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 0,
